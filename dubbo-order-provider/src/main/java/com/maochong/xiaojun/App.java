c70a599b62c39756c6f27d8a6836f8815e60a47c
luyj <jokin@tongbu.com>
2018-04-27 07:56:03 +0800

    添加dubbo测试项目

---
+++ dubbo-order-provider/src/main/java/com/maochong/xiaojun/App.java
@@ -0,0 +1,13 @@
+package com.maochong.xiaojun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
